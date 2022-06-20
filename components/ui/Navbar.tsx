c162478b91c68ab06004063255fb575c64bb15a3
Mauro <molinamauro2@gmail.com>
2022-06-20 20:57:15 -0300

    finished pokemon detail page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 import Image from "next/image";
 import React from "react";
 
@@ -18,14 +18,29 @@ export const Navbar = () => {
         width: "100%",
       }}
     >
-        <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-        alt="App Icon" width={70} height={70}/>
-      <Text color="white" h2>P</Text>
-      <Text color="white" h3>okémon</Text>
+      <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        alt="App Icon"
+        width={70}
+        height={70}
+      />
+      <Link href="/">
+        <Text color="white" h2>
+          P
+        </Text>
+      </Link>
+      <Link href="/">
+        <Text color="white" h3>
+          okémon
+        </Text>
+      </Link>
 
-      
-      <Spacer css={{flex: 1}}/>
-      <Text color="white" h3>Favoritos</Text>
+      <Spacer css={{ flex: 1 }} />
+      <Link href="/favorites" css={{ marginRight: "10px" }}>
+        <Text color="white" h3>
+          Favoritos
+        </Text>
+      </Link>
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@nextui-org/react'
+import { Layout } from '../../components/layouts'
+
+const FavoritesPage = () => {
+  return (
+    <Layout title='Mis Favoritos - Pokémon App'>
+        <Text h1>Mis Favoritos</Text>
+    </Layout>
+  )
+}
+
+export default FavoritesPage;
