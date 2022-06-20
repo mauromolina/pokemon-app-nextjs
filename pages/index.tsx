fb90fe52dae7801f30009d6df6a95ab17bef4597
Mauro <molinamauro2@gmail.com>
2022-06-20 02:43:18 -0300

    added _document.tsx and nextui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const HomePage: NextPage = () => {
   return (
-    <h1>PokeApp con Next</h1>
+    <>
+      <h1>PokeApp con Next</h1>
+      <Button>Hola!</Button>
+  
+    </>
   )
 }
 
